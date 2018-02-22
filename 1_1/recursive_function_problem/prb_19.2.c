5e6b4fc323dd0055a5e3123d4aef20623e988433
kryptonpust <nafiul.ice.pust@gmail.com>
2018-02-22 21:01:35 +0600

    added some new solve

--- 1_1/recursive_function_problem/prb_19.2.c
+++ 1_1/recursive_function_problem/prb_19.2.c
@@ -6,17 +6,11 @@ fibonacci number*/
 int fibo(int num) {
 
     if (num == 0) {
-
         return 0;
-
     } else if (num == 1) {
-
         return 1;
-
     } else {
-
         return (fibo(num - 1) + fibo(num - 2));
-
     }
 
 }
